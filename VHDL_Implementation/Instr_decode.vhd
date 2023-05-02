8404eaf2fb6c54506bcc0876d2e3b6273caf4024
Ishaan Manhar <210070033@iitb.ac.in>
2023-05-02 17:11:30 +0530

    Update Instr_decode.vhd

--- VHDL_Implementation/Instr_decode.vhd
+++ VHDL_Implementation/Instr_decode.vhd
@@ -7,21 +7,20 @@ entity instr_decode is
     (
         PC_in : in std_logic_vector(15 downto 0);
         Instruction : in std_logic_vector(15 downto 0);
-        Ra,Rb,Rc ,Alu_sel : out std_logic_vector(2 downto 0) := "000";
-        Imm_out : out std_logic_vector(15 downto 0);
-        rf_wr, c_modify, z_modify, mem_wr, mem_mux, imm_mux : out std_logic := '0';
-        opcode : out std_logic_vector(3 downto 0);
-        Last2: out std_logic_vector(1 downto 0);
+        out_RegA,out_RegB,out_RegC ,out_Alu_sel : out std_logic_vector(2 downto 0);
+        out_Imm_out : out std_logic_vector(15 downto 0);
+        out_rf_wr, out_c_modify, out_z_modify, out_mem_wr, out_mem_mux, out_imm_mux : out std_logic;
+        out_opcode : out std_logic_vector(3 downto 0);
+        out_Last2: out std_logic_vector(1 downto 0);
         PC_BP : out std_logic_vector(15 downto 0);
-        LM_SM_hazard : out std_logic;
-        counter_out : out std_logic_vector(2 downto 0);
-		mera_mux : out std_logic_vector(1 downto 0);
+        out_LM_SM_hazard : out std_logic;
+		out_mera_mux : out std_logic_vector(1 downto 0);
 		  clk: in std_logic
     );
 end entity;
 
 architecture instr_decode_arch of instr_decode is
-
+		shared variable count : integer := 0;
 		component SE7 is
 		port (Raw: in std_logic_vector(8 downto 0 );
 			 Outp:out std_logic_vector(15 downto 0):="0000000000000000");
@@ -38,11 +37,12 @@ architecture instr_decode_arch of instr_decode is
 			 DataOut:out std_logic_vector(2 downto 0));
 		end component Register_3bit;
 
-		component ID_adder is
-			port(   PC,Imm9: in std_logic_vector(15 downto 0);
-						 BranchedPC: out std_logic_vector(15 downto 0)
-				);
-		end component;
+		component adder is
+	port( 
+	    Inp1,Inp2: in std_logic_vector(15 downto 0);
+		Outp: out std_logic_vector(15 downto 0)
+		);
+end component adder;
 	component Mux16_4x1 is
     port(A0: in std_logic_vector(15 downto 0);
          A1: in std_logic_vector(15 downto 0);
@@ -54,300 +54,369 @@ architecture instr_decode_arch of instr_decode is
 
 
 		signal Imm6_out,Imm9_out : std_logic_vector(15 downto 0) := "0000000000000000";
-		signal counter_in : std_logic_vector(2 downto 0) := "000";
-		signal count : integer := 0;
+		signal counter_in,counter_out : std_logic_vector(3 downto 0) := "0000";
 		
 	begin 
 
 		Sign_6 : SE10 port map(Instruction(5 downto 0),Imm6_out);
 		Sign_9 : SE7 port map(Instruction(8 downto 0),Imm9_out);
-		ad: ID_adder port map(PC_in,Imm9_out,PC_BP);
-		counter: Register_3bit port map(counter_in,clk,'1',counter_out);
+		ad: adder port map(PC_in,Imm9_out,PC_BP);
+		counter: Register_4bit port map(counter_in,clk,'1',counter_out);
 		
-		instr : process(clk)
-		
-			begin 
-			mera_mux <= "00";
-			Last2 <= Instruction(1 downto 0);
-			opcode <= Instruction(15 downto 12);
-			Ra <= Instruction(11 downto 9);
-			Rb <= Instruction(8 downto 6);
-			Rc <= Instruction(5 downto 3);
-			
-			Imm_out <= "0000000000000000";
-			
+		instr : process(clk,Instruction,Imm6_out,Imm9_out,counter_in) 
+			variable RegA : std_logic_vector(2 downto 0);
+			variable RegB : std_logic_vector(2 downto 0);
+			variable RegC : std_logic_vector(2 downto 0);
+			variable Alu_sel : std_logic_vector(2 downto 0);
+         variable Imm_out : std_logic_vector(15 downto 0);
+         variable rf_wr, c_modify, z_modify, mem_wr, mem_mux, imm_mux : std_logic;
+        variable opcode : std_logic_vector(3 downto 0);
+        variable Last2: std_logic_vector(1 downto 0);
+        variable LM_SM_hazard : std_logic;
+		  variable mera_mux : std_logic_vector(1 downto 0);
+		  begin
+			rf_wr := '0';
+			c_modify := '0';
+			z_modify := '0';
+			mem_wr := '0';
+			mem_mux := '0';
+			imm_mux := '0';
+			mera_mux := "00";
+			Last2 := Instruction(1 downto 0);
+			opcode := Instruction(15 downto 12);
+			RegA := Instruction(11 downto 9);
+			RegB := Instruction(8 downto 6);
+			RegC := Instruction(5 downto 3);
+			Alu_sel := "000";
+			Imm_out := "0000000000000000";
+			LM_SM_hazard := '0';
 -----------------ADI--------------------------------
 			if(Instruction(15 downto 12) = "0000") then   
-				 Imm_out <= Imm6_out;
-				 c_modify <= '1';
-				 z_modify <= '1';
-				 rf_wr <= '1';
-				 Alu_sel <= "000";--add ka 000
-				 mem_wr <= '0';
-				 mem_mux <= '0';
-				 imm_mux <= '1';
+				 Imm_out := Imm6_out;
+				 c_modify := '1';
+				 z_modify := '1';
+				 rf_wr := '1';
+				 Alu_sel := "000";--add ka 000
+				 mem_wr := '0';
+				 mem_mux := '0';
+				 imm_mux := '1';
  -----------ADD ke baki saare-----------------------
 			elsif(Instruction(15 downto 12) = "0001") then
-				mera_mux <= Instruction(1 downto 0);
-				 c_modify <= '1';
-				 z_modify <= '1';
-				 rf_wr <= '1';
-				 Alu_sel(1 downto 0) <= "00"; --add ka 000
-				 Alu_sel(2) <= Instruction (2);
-				 mem_wr <= '0';
-				 mem_mux <= '0';
-				 imm_mux <= '0';
+				mera_mux := Instruction(1 downto 0);
+				 c_modify := '1';
+				 z_modify := '1';
+				 rf_wr := '1';
+				 Alu_sel(1 downto 0) := "00"; --add ka 000
+				 Alu_sel(2) := Instruction (2);
+				 mem_wr := '0';
+				 mem_mux := '0';
+				 imm_mux := '0';
 
 			elsif(Instruction(15 downto 12) = "0010") then
-				mera_mux <= Instruction(1 downto 0);
-				 c_modify <= '1';
-				 z_modify <= '1';
-				 rf_wr <= '1';
-				 Alu_sel(1 downto 0) <= "01"; --nand_ka 001
-				 Alu_sel(2) <= PC(2);
-				 PCr <= '1';
-				 mem_wr <= '0';
-				 mem_mux <= '0';
-				 imm_mux <= '0';
+				mera_mux := Instruction(1 downto 0);
+				 c_modify := '1';
+				 z_modify := '1';
+				 rf_wr := '1';
+				 Alu_sel(1 downto 0) := "01"; --nand_ka 001
+				 Alu_sel(2) := Instruction(2);
+				 mem_wr := '0';
+				 mem_mux := '0';
+				 imm_mux := '0';
 
 			elsif(Instruction(15 downto 12) = "0011" or Instruction(15 downto 12) = "0100") then
-				 z_modify <= Instruction(14);
-				 rf_wr <= '1';
-				 mem_wr <= '0';
-				 mem_mux <= '1';
-				 imm_mux <= '1'; 
-				 if(PC(15 downto 12) = "0011") then
-					  Imm_out <= Imm9_out;
+				 z_modify := Instruction(14);
+				 rf_wr := '1';
+				 mem_wr := '0';
+				 mem_mux := '1';
+				 imm_mux := '1'; 
+				 if(Instruction(15 downto 12) = "0011") then
+					  Imm_out := Imm9_out;
 				 else
-					  Imm_out <= Imm6_out;
+					  Imm_out := Imm6_out;
 				 end if;  
 
 			elsif(Instruction(15 downto 12) = "0101") then
-				 Imm_out <= Imm6_out;
-				 rf_wr <= '0';
-				 mem_wr <= '1';
-				 imm_mux <= '1'; 
+				 Imm_out := Imm6_out;
+				 rf_wr := '0';
+				 mem_wr := '1';
+				 imm_mux := '1'; 
 
 
 
 			elsif(Instruction(15 downto 12) = "0110") then
-				 rf_wr <= '1';
-				 mem_mux <= '1';
-				 imm_mux <= '1';
-				 if  (counter_in = "000") then
-					  counter_in <= "001";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "111";
-					
-							count := count + 1; 
-					
-				 elsif (counter_in = "001") then
-					  counter_in <= "010";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "110";
-					 
-							count := count + 1; 
-					
-				 elsif (counter_in = "010") then
-					  counter_in <= "011";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "101";
+				 rf_wr := '0';
+				 mem_mux := '1';
+				 imm_mux := '1';
+				 if  (counter_out= "0000") then
+					  counter_in <= "0001";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "111";
+					  if(Instruction(0) = '1') then
+							rf_wr := '1';
+					  end if;
+						
 					  
-							count := count + 1; 
-					
-				 elsif (counter_in = "011") then
-					  counter_in <= "100";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "100";
+				 elsif (counter_out = "0001") then
+					  counter_in <= "0010";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "110";
+					   if(Instruction(1) = '1') then
+							rf_wr := '1';
+					  end if;
 					  
-							count := count + 1;
-				
-				 elsif (counter_in = "100") then
-					  counter_in <= "101";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "100";
-					 
-							count := count + 1;
 					
-				 elsif (counter_in = "101") then
-					  counter_in <= "110";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "011";
-					 
-							count := count + 1;
+				 elsif (counter_out = "0010") then
+					  counter_in <= "0011";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "101";
+					   if(Instruction(2) = '1') then
+							rf_wr := '1';
+					  end if;
 					
-				 elsif (counter_in = "110") then
-					  counter_in <= "111";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "010";
-					 
-							count := count + 1;
+				 elsif (counter_out = "0011") then
+					  counter_in <= "0100";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "100";
+					   if(Instruction(3) = '1') then
+							rf_wr := '1';
+					  end if;
+					  
+	
+				 elsif (counter_out = "0100") then
+					  counter_in <= "0101";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "011";
+					   if(Instruction(4) = '1') then
+							rf_wr := '1';
+					  end if;
+
+					
+				 elsif (counter_out = "0101") then
+					  counter_in <= "0110";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "010";
+					   if(Instruction(5) = '1') then
+							rf_wr := '1';
+					  end if;
+	
+					
+				 elsif (counter_out = "0110") then
+					  counter_in <= "0111";
+					  LM_SM_hazard := '1';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "001";
+					   if(Instruction(6) = '1') then
+							rf_wr := '1';
+					  end if;
 				
-				 elsif (counter_in = "111") then
-					  counter_in <= "000";
-					  LM_SM_hazard <= '0';
-					  opcode <= "0100";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "010";
-					 
-							count := count + 1;
+				 elsif (counter_out = "0111") then
+					  counter_in <= "0000";
+					  LM_SM_hazard := '0';
+					  opcode := "0100";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "000";
+					  if(Instruction(7) = '1') then
+							rf_wr := '1';
+					  end if;
+
+				else
+					counter_in <= "0000";
+					opcode := Instruction(15 downto 12);
+					RegA := Instruction(11 downto 9);
+					RegB := Instruction(8 downto 6);
+					RegC := Instruction(5 downto 3);
+
 					
 				 end if;
 				 
-				 case count is
-					when 0 =>
-						Imm_out<="0000000000000000";
-					when 1 =>
-						Imm_out<="0000000000000010";
-					when 2 =>
-						Imm_out<="0000000000000100";
-					when 3 =>
-						Imm_out<="0000000000000110";
-					when 4 =>
-						Imm_out<="0000000000001000";
-					when 5 =>
-						Imm_out<="0000000000001010";
-					when 6 =>
-						Imm_out<="0000000000001100";
+				 case counter_out is
+					when "0000" =>
+						Imm_out:="0000000000000000";
+					when "0001"=>
+						Imm_out:="0000000000000010";
+					when "0010"=>
+						Imm_out:="0000000000000100";
+					when "0011"=>
+						Imm_out:="0000000000000110";
+					when "0100"=>
+						Imm_out:="0000000000001000";
+					when "0101" =>
+						Imm_out:="0000000000001010";
+					when "0110" =>
+						Imm_out:="0000000000001100";
+					when "0111" =>
+						Imm_out:="0000000000001110";
 					when others =>
-						Imm_out<="0000000000001110";
+						Imm_out:=Imm9_out;
 					end case;
 						
 
 			elsif (Instruction(15 downto 12) = "0111") then
-				 rf_wr <= '1';
-				 mem_mux <= '1';
-				 imm_mux <= '1';
-				 if (counter_in = "000") then
-					  counter_in <= "001";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "111";
-					 
-							count := count + 1; 
-				
-				 elsif (counter_in = "001") then
-					  counter_in <= "010";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "110";
-					 
-							count := count + 1; 
-					
-				 elsif (counter_in = "010") then
-					  counter_in <= "011";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "101";
-					  
-							count := count + 1; 
-					
-				 elsif (counter_in = "011") then
-					  counter_in <= "100";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "100";
-					 
-							count := count + 1;
-				
-				 elsif (counter_in = "100") then
-					  counter_in <= "101";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "100";
-					  
-							count := count + 1;
-				
-				 elsif (counter_in = "101") then
-					  counter_in <= "110";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "011";
-					  
-							count := count + 1;
+				 mem_wr := '0';
+				 mem_mux := '1';
+				 imm_mux := '1';
+				 if (counter_out = "0000") then
+					  counter_in <= "0001";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "111";
+					   if(Instruction(0) = '1') then
+							mem_wr := '1';
 					  end if;
-				 elsif (counter_in = "110") then
-					  counter_in <= "111";
-					  LM_SM_hazard <= '1';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "010";
-					  
-							count := count + 1;
+					 
+				 elsif (counter_out = "0001") then
+					  counter_in <= "0010";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "110";
+					  if(Instruction(1) = '1') then
+							mem_wr := '1';
+					  end if;
+					 
 					
-				 elsif (counter_in = "111") then
-					  counter_in <= "000";
-					  LM_SM_hazard <= '0';
-					  opcode <= "0101";
-					  RegB <= Instruction(12 downto 9);
-					  RegA <= "010";
 					
-							count := count + 1;
+				 elsif (counter_out = "0010") then
+					  counter_in <= "0011";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "101";
+					  if(Instruction(2) = '1') then
+							mem_wr := '1';
+					  end if;
+				
+					
+				 elsif (counter_out = "0011") then
+					  counter_in <= "0100";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "100";
+					  if(Instruction(3) = '1') then
+							mem_wr := '1';
+					  end if;
+				 elsif (counter_out = "0100") then
+					  counter_in <= "0101";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "100";
+				     if(Instruction(4) = '1') then
+							mem_wr := '1';
+					  end if;
+				 elsif (counter_out = "0101") then
+					  counter_in <= "0110";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "011";
+					  if(Instruction(5) = '1') then
+							mem_wr := '1';
+					  end if;
+
+				 elsif (counter_out = "0110") then
+					  counter_in <= "0111";
+					  LM_SM_hazard := '1';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "010";
+					  if(Instruction(6) = '1') then
+							mem_wr := '1';
+					  end if;
+					
+				 elsif (counter_out = "0111") then
+					  counter_in <= "0000";
+					  LM_SM_hazard := '0';
+					  opcode := "0101";
+					  RegB := Instruction(11 downto 9);
+					  RegA := "010";
+				     if(Instruction(7) = '1') then
+							mem_wr := '1';
+					  end if;
+				else
+					counter_in <= "0000";
+					opcode := Instruction(15 downto 12);
+					RegA := Instruction(11 downto 9);
+					RegB := Instruction(8 downto 6);
+					RegC := Instruction(5 downto 3);
+
 				
 				 end if;
-				case count is
-					when 0 =>
-						Imm_out<="0000000000000000";
-					when 1 =>
-						Imm_out<="0000000000000010";
-					when 2 =>
-						Imm_out<="0000000000000100";
-					when 3 =>
-						Imm_out<="0000000000000110";
-					when 4 =>
-						Imm_out<="0000000000001000";
-					when 5 =>
-						Imm_out<="0000000000001010";
-					when 6 =>
-						Imm_out<="0000000000001100";
-					when others =>
-						Imm_out<="0000000000001110";
+				case counter_out is
+					when "0000" =>
+						Imm_out:="0000000000000000";
+					when "0001"=>
+						Imm_out:="0000000000000010";
+					when "0010"=>
+						Imm_out:="0000000000000100";
+					when "0011" =>
+						Imm_out:="0000000000000110";
+					when "0100"=>
+						Imm_out:="0000000000001000";
+					when "0101" =>
+						Imm_out:="0000000000001010";
+					when "0110" =>
+						Imm_out:="0000000000001100";
+					when "0111" =>
+						Imm_out:="0000000000001110";
+					when others=>
+						Imm_out:= Imm9_out;
 					end case;
 
 			elsif(Instruction(15 downto 12) = "1000" or Instruction(15 downto 12) = "1001" or Instruction(15 downto 12) = "1010") then
-				Imm_out <= Imm6_out;
-				 rf_wr <= '1';
-				 ALU_sel <= "010"; --sub ka 010
-				 mem_mux <= '0';
-				 imm_mux <= '1';
+				Imm_out := Imm6_out;
+				 rf_wr := '1';
+				 ALU_sel := "010"; --sub ka 010
+				 mem_mux := '0';
+				 imm_mux := '1';
 
 
 			elsif(Instruction(15 downto 12) = "1100") then
-				 Imm_out <= Imm9_out;
-				 rf_wr <= '1';
-				 imm_mux <= '0';
+				 Imm_out := Imm9_out;
+				 rf_wr := '1';
+				 imm_mux := '0';
 
 			elsif(Instruction(15 downto 12) = "1101") then
-				 rf_wr <= '1';
-				 Alu_sel <= "000"; --add ka 000
-				 mem_wr <= '0';
-				 mem_mux <= '0';
-				 imm_mux <= '1';
+				 rf_wr := '1';
+				 Alu_sel := "000"; --add ka 000
+				 mem_wr := '0';
+				 mem_mux := '0';
+				 imm_mux := '1';
 
 			elsif(Instruction(15 downto 12) = "1111") then
-				 rf_wr <= '1';
-				 mem_mux <= '0';
-				 imm_mux <= '1'; 
+				 rf_wr := '1';
+				 mem_mux := '0';
+				 imm_mux := '1'; 
 			end if;
+			out_rf_wr <= rf_wr;
+			out_c_modify <= c_modify;
+			out_z_modify <= z_modify;
+			out_mem_wr <= mem_wr;
+			out_mem_mux <= mem_mux;
+			out_imm_mux <= imm_mux;
+			out_mera_mux <= mera_mux;
+			out_Last2 <= Last2;
+			out_opcode <= opcode;
+			out_RegA <= RegA;
+			out_RegB <= RegB;
+			out_RegC <= RegC;
+			out_Alu_sel <= Alu_sel;
+			out_Imm_out <= Imm_out;
+			out_LM_SM_hazard <= LM_SM_hazard;
 			end process;
 end architecture;
